3dc735fbb5284859c6b56b95f1d57571ad204b66
Debora <157063951+debs-sa@users.noreply.github.com>
2024-11-25 13:03:36 -0700

    Add files via upload
    projeto upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace WeatherForecast
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtmaxtemp = new System.Windows.Forms.TextBox();
+            this.txtmintemp = new System.Windows.Forms.TextBox();
+            this.txthumidity = new System.Windows.Forms.TextBox();
+            this.txtwindk = new System.Windows.Forms.TextBox();
+            this.txtcity = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtcurrenttemp = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(185, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(57, 366);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Max temp (C)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(265, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Min temp (C)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(627, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Wind Speed (m/s)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(460, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Humidity (%)";
+            // 
+            // txtmaxtemp
+            // 
+            this.txtmaxtemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmaxtemp.Location = new System.Drawing.Point(45, 409);
+            this.txtmaxtemp.Name = "txtmaxtemp";
+            this.txtmaxtemp.Size = new System.Drawing.Size(135, 30);
+            this.txtmaxtemp.TabIndex = 5;
+            // 
+            // txtmintemp
+            // 
+            this.txtmintemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmintemp.Location = new System.Drawing.Point(251, 409);
+            this.txtmintemp.Name = "txtmintemp";
+            this.txtmintemp.Size = new System.Drawing.Size(135, 30);
+            this.txtmintemp.TabIndex = 6;
+            // 
+            // txthumidity
+            // 
+            this.txthumidity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txthumidity.Location = new System.Drawing.Point(444, 409);
+            this.txthumidity.Name = "txthumidity";
+            this.txthumidity.Size = new System.Drawing.Size(135, 30);
+            this.txthumidity.TabIndex = 7;
+            // 
+            // txtwindk
+            // 
+            this.txtwindk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtwindk.Location = new System.Drawing.Point(631, 409);
+            this.txtwindk.Name = "txtwindk";
+            this.txtwindk.Size = new System.Drawing.Size(135, 30);
+            this.txtwindk.TabIndex = 8;
+            // 
+            // txtcity
+            // 
+            this.txtcity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcity.Location = new System.Drawing.Point(278, 37);
+            this.txtcity.Name = "txtcity";
+            this.txtcity.Size = new System.Drawing.Size(245, 30);
+            this.txtcity.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(348, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(127, 104);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(386, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Cloud";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkViolet;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(561, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 56);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Show weather";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(230, 473);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(555, 257);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkViolet;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(31, 567);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 78);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Search Weather Forecast";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtcurrenttemp
+            // 
+            this.txtcurrenttemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcurrenttemp.Location = new System.Drawing.Point(353, 290);
+            this.txtcurrenttemp.Name = "txtcurrenttemp";
+            this.txtcurrenttemp.Size = new System.Drawing.Size(135, 30);
+            this.txtcurrenttemp.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(352, 258);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(136, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Current temp (C)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 779);
+            this.Controls.Add(this.txtcurrenttemp);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtcity);
+            this.Controls.Add(this.txtwindk);
+            this.Controls.Add(this.txthumidity);
+            this.Controls.Add(this.txtmintemp);
+            this.Controls.Add(this.txtmaxtemp);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyWeatherForecast";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtmaxtemp;
+        private System.Windows.Forms.TextBox txtmintemp;
+        private System.Windows.Forms.TextBox txthumidity;
+        private System.Windows.Forms.TextBox txtwindk;
+        private System.Windows.Forms.TextBox txtcity;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtcurrenttemp;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Net;
+using System.Xml.Linq;
+using System.Diagnostics;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace WeatherForecast
+{
+    public partial class Form1 : Form
+    {
+        string apiKey = "dd25fbbb5bc592da35d856662e9222c7";
+        ClassResponseWeather myWeather;
+        double lon;
+        double lat;
+        ClassForecast5Days myForecast5Days;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = $"https://api.openweathermap.org/data/2.5/weather?q={txtcity.Text}&appid={apiKey}";
+                var json = web.DownloadString(url);
+                Debug.WriteLine(json);
+                myWeather = JsonConvert.DeserializeObject<ClassResponseWeather>(json);
+            }
+
+            string currenttemp = Math.Round(myWeather.main.temp - 273.15, 0).ToString();
+            string maxtemp = Math.Round(myWeather.main.temp_max-273.15, 0).ToString();
+            string mintemp = Math.Round(myWeather.main.temp_min-273.15, 0).ToString();
+            string wind = myWeather.wind.speed.ToString();
+            string humidity = myWeather.main.humidity.ToString();
+            string cloud = myWeather.weather[0].description.ToString();
+
+            txtcurrenttemp.Text = currenttemp;
+            txtmaxtemp.Text = maxtemp;
+            txtmintemp.Text = mintemp;
+            txtwindk.Text = wind;
+            txthumidity.Text = humidity;
+            label6.Text = cloud;
+            pictureBox1.ImageLocation = "https://openweathermap.org/img/w/" + myWeather.weather[0].icon + ".png";
+
+            lon = myWeather.coord.lon;
+            lat = myWeather.coord.lat;
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Date", typeof(string));
+            dt.Columns.Add("Min Temp (C)", typeof(string));
+            dt.Columns.Add("Mean Temp (C)", typeof(string));
+            dt.Columns.Add("Max Temp (C)", typeof(string));
+            dt.Columns.Add("Humidity (%)", typeof(string));
+            dt.Columns.Add("Cloud", typeof(string));
+
+            
+
+            using (WebClient web = new WebClient())
+            {
+                string uri = $"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}";
+                var json = web.DownloadString(uri);
+                Debug.WriteLine($"response: {json}");
+                myForecast5Days = JsonConvert.DeserializeObject<ClassForecast5Days>(json);
+            }
+
+            foreach (var npc in myForecast5Days.list)
+            {
+                dt.Rows.Add(new object[]
+                {
+                    npc.dt_txt,
+                    Math.Round(npc.main.temp_min-273.15, 0),
+                    Math.Round(npc.main.temp-273.15, 0),
+                    Math.Round(npc.main.temp_max-273.15, 0),
+                    npc.main.humidity,
+                    npc.weather[0].description
+                });
+            }
+
+            dataGridView1.DataSource = dt;
+
+        }
+    }
+}
